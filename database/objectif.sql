a0420ff0fa734aec794764d9fdcc7c1761978d74
Hachim02Pox <164426662+Hachim02Pox@users.noreply.github.com>
2026-03-03 16:40:36 -0500

    Delete database/objectif.sql

--- database/objectif.sql
+++
@@ -1,8 +0,0 @@
-SHOW DATABASES;
-
-CREATE TABLE Objectif (
-    id_objectif INT PRIMARY KEY AUTO_INCREMENT,
-    id_utilisateur INT NOT NULL,
-    type_objectif VARCHAR(100) NOT NULL,
-    statut VARCHAR(20) NOT NULL
-);

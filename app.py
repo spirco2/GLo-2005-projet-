3cd9b12371eaed29d6df283e1ba20364edd8f181
melji <meljishimada@gmail.com>
2026-04-10 21:54:29 -0400

    fin du code

--- app.py
+++ app.py
@@ -68,12 +68,15 @@ from routes.explorer import bp as explorer_bp
 from routes.securite import bp as securite_bp
 from routes.programmes import bp as programmes_bp
 from routes.suivi import bp as suivi_bp
+from routes.admin import bp as admin_bp
+
 
 app.register_blueprint(acceuil_bp)
 app.register_blueprint(explorer_bp)
 app.register_blueprint(securite_bp)
 app.register_blueprint(programmes_bp)
 app.register_blueprint(suivi_bp)
+app.register_blueprint(admin_bp)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
